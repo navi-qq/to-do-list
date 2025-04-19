554e1ab7b1af94b27429cecc5bd6662a292a3670
navi-qq <201188602+navi-qq@users.noreply.github.com>
2025-04-19 18:01:23 +0800

    Separate variable declaration & add render method

--- app/src/main/java/com/appdev/todolist/MainActivity.java
+++ app/src/main/java/com/appdev/todolist/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,13 +18,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    List<String> tasks = new ArrayList<>();
+    ImageView addTaskBtn;
+    LinearLayout taskContainer;
     LinearLayout container;
     CheckBox checkbox;
     EditText editText;
     ImageView pencilIcon;
     ImageView deleteIcon;
+    LayoutParams containerParams;
+    LayoutParams iconParams;
+    MarginLayoutParams editTextParams;
+
+    int centerPosition = Gravity.CENTER;
+    int iconSize;
+    int editTextSize;
+    int pencilIconMargin;
 
 
     @Override
@@ -37,56 +52,25 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        addTaskBtn = findViewById(R.id.addTaskBtn);
+        taskContainer = findViewById(R.id.taskContainer);
 
-        int iconDp = 22;
-        int editTextDp = 250;
-        int iconSize = dpConverter(iconDp);
-        int editTextSize = dpConverter(editTextDp);
-        int pencilIconMargin = dpConverter(8);
+        iconSize = dpConverter(22);
+        editTextSize = dpConverter(250);
+        pencilIconMargin = dpConverter(8);
 
-        LinearLayout.LayoutParams containerParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.HORIZONTAL);
-        LinearLayout.LayoutParams iconParams = new LinearLayout.LayoutParams(iconSize, iconSize);
-        MarginLayoutParams editTextParams = new MarginLayoutParams(editTextSize, LinearLayout.LayoutParams.WRAP_CONTENT);
+        containerParams = new LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.HORIZONTAL);
+        iconParams = new LayoutParams(iconSize, iconSize);
+        editTextParams = new MarginLayoutParams(editTextSize, LinearLayout.LayoutParams.WRAP_CONTENT);
 
-        int centerPosition = Gravity.CENTER;
         iconParams.gravity = centerPosition;
         containerParams.gravity = centerPosition;
-
-        iconParams.setMargins(pencilIconMargin,0,pencilIconMargin,0);
-
-        ImageView addTaskBtn = findViewById(R.id.addTaskBtn);
-        LinearLayout taskContainer = findViewById(R.id.taskContainer);
-
-
+        iconParams.setMargins(pencilIconMargin,0, pencilIconMargin,0);
 
         addTaskBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                container = new LinearLayout(getApplicationContext());
-                checkbox = new CheckBox(getApplicationContext());
-                editText = new EditText(getApplicationContext());
-                pencilIcon = new ImageView(getApplicationContext());
-                deleteIcon = new ImageView(getApplicationContext());
-
-                container.setLayoutParams(containerParams);
-
-                editText.setHint("Untitled Task");
-                editText.setLayoutParams(editTextParams);
-
-                pencilIcon.setImageResource(R.drawable.pencil_icon);
-                pencilIcon.setLayoutParams(iconParams);
-
-                deleteIcon.setImageResource(R.drawable.trash_icon);
-                deleteIcon.setLayoutParams(iconParams);
-
-                container.addView(checkbox);
-                container.addView(editText);
-                container.addView(pencilIcon);
-                container.addView(deleteIcon);
-
-                taskContainer.addView(container);
-
+                render();
             }
         });
     }
@@ -94,5 +78,34 @@ public class MainActivity extends AppCompatActivity {
     public int dpConverter(int dps) {
         float scale = getResources().getDisplayMetrics().density;
         return (int) (dps * scale + 0.5f);
+    }
+
+    public void render() {
+        taskContainer.removeAllViews();
+        tasks.forEach(task -> {
+            container = new LinearLayout(getApplicationContext());
+            checkbox = new CheckBox(getApplicationContext());
+            editText = new EditText(getApplicationContext());
+            pencilIcon = new ImageView(getApplicationContext());
+            deleteIcon = new ImageView(getApplicationContext());
+
+            container.setLayoutParams(containerParams);
+
+            editText.setText(task);
+            editText.setLayoutParams(editTextParams);
+
+            pencilIcon.setImageResource(R.drawable.pencil_icon);
+            pencilIcon.setLayoutParams(iconParams);
+
+            deleteIcon.setImageResource(R.drawable.trash_icon);
+            deleteIcon.setLayoutParams(iconParams);
+
+            container.addView(checkbox);
+            container.addView(editText);
+            container.addView(pencilIcon);
+            container.addView(deleteIcon);
+
+            taskContainer.addView(container);
+        });
     }
 }
