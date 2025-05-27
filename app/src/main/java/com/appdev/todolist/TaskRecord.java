4796868c26af286d8ed1463cb9dae91948389dba
navi-qq <201188602+navi-qq@users.noreply.github.com>
2025-05-27 13:02:12 +0800

    Add TaskRecord activity

--- app/src/main/java/com/appdev/todolist/TaskRecord.java
+++ app/src/main/java/com/appdev/todolist/TaskRecord.java
@@ -2,19 +2,32 @@ package com.appdev.todolist;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskRecord extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(null);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_task_record);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -23,6 +36,31 @@ public class TaskRecord extends AppCompatActivity {
             return insets;
         });
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        ListView listView = findViewById(R.id.taskRecordList);
+        ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<>(TaskRecord.this, R.layout.task_record_list, list);
+        listView.setAdapter(adapter);
+
+
+        database.getReference().child("Tasks").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+
+                    TaskHelper taskHelper = snapshot.getValue(TaskHelper.class);
+                    String text = "TASK: " + taskHelper.getTaskDescription().toUpperCase() + " \nStatus: " + taskHelper.getTaskStatus();
+                    list.add(text);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         ImageView backButton = findViewById(R.id.backButton);
 
